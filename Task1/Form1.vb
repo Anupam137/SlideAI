cc82591f6b9a68e48a3fc99d35ab525912070b9b
Anupam137 <131847192+Anupam137@users.noreply.github.com>
2024-06-13 14:23:51 +0530

    Add project files.

---
+++ Task1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        MsgBox("Anupam Jha here, this is my Slidely AI task")
+    End Sub
+End Class
